4da5180f00f592893e7cf27cfde89adebba215c8
Kyle McGrath <kylemcgrath@gmail.com>
2020-03-13 10:24:47 -0400

    Fix dialog first appearance.

--- src/view/main/dialog.cpp
+++ src/view/main/dialog.cpp
@@ -9,6 +9,21 @@ Dialog::Dialog(QWidget *parent) :
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+    QSettings settings(tr("MusicalGraph"), tr("MusicalGraph"), this);
+    QString gameVersion;
+    if(settings.contains(tr("gameVersion")))
+        gameVersion = settings.value(tr("gameVersion")).toString();
+    else {
+        settings.setValue(tr("gameVersion"), tr("easy"));
+        gameVersion = tr("easy");
+    }
+
+    if(gameVersion == tr("hard"))
+        ui->radioButton_3->setChecked(true);
+    else if(gameVersion == tr("medium"))
+        ui->radioButton_2->setChecked(true);
+    else
+        ui->radioButton->setChecked(true);
 }
 
 Dialog::~Dialog()
@@ -21,24 +36,11 @@ void Dialog::on_buttonBox_accepted()
 
     QSettings settings(tr("MusicalGraph"), tr("MusicalGraph"), this);
     QString gameVersion;
-    if(settings.contains(tr("gameVersion")))
-        gameVersion = settings.value(tr("gameVersion")).toString();
-    else {
-        settings.setValue(tr("gameVersion"), tr("easy"));
-        gameVersion = tr("easy");
-    }
-
-    if(gameVersion == tr("hard"))
-        ui->radioButton->setChecked(true);
-    else if(gameVersion == tr("medium"))
-        ui->radioButton_2->setChecked(true);
-    else
-        ui->radioButton_3->setChecked(true);
     if(ui->radioButton->isChecked())
         settings.setValue(tr("gameVersion"),"easy");
     else if(ui->radioButton_2->isChecked())
         settings.setValue(tr("gameVersion"),"medium");
-    else if(ui->radioButton_3->isChecked()) {
-        settings.setValue(tr("gameVersion"),"hard");}
+    else if(ui->radioButton_3->isChecked())
+        settings.setValue(tr("gameVersion"),"hard");
     hide();
 }
